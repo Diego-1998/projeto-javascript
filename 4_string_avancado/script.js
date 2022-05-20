3eb78346bf0163c5bffe666eed02a3a38d581f13
Diego leandro <diegolgaspar000@gmail.com>
2022-05-19 23:24:01 -0300

    String_part 2

---
+++ 4_string_avancado/script.js
@@ -0,0 +1,4 @@
+console.log("Meu nome é Diego \n e tenho 30 anos de idade " );
+console.log('Quero ganhar um carro do modelo "Volvo" ');
+console.log("Eu gosto de café e também gosto de 'Ice Tea'");
+console.log(`A multiplicação de 15 por 3 é ${15 * 3}`);
