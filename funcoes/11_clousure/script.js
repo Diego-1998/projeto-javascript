6df613d6aefb2f4e222b19f2567729a408376820
Diego leandro <diegolgaspar000@gmail.com>
2022-05-26 23:55:48 -0300

    Closure Javascript

---
+++ funcoes/11_clousure/script.js
@@ -0,0 +1,19 @@
+function lembrarSoma(x){
+    return function(y) {
+        return x + y;
+    }
+}
+
+let soma1 = lembrarSoma(3);
+console.log(soma1(5));
+
+let soma2 = lembrarSoma(10);
+console.log(soma2(3));
+function contador(i) {
+    let cont = i;
+    let somarContador = function(){
+        console.log(cont);
+        cont++;
+    }
+    return somarContador;
+}
