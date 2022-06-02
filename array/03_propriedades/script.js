18bb0221382f6cda22247db3daaf2bdecb085905
Diego leandro <diegolgaspar000@gmail.com>
2022-06-01 23:00:24 -0300

    Metodos

--- array/03_propriedades/script.js
+++ array/03_propriedades/script.js
@@ -5,5 +5,5 @@ console.log(nomes.length);
 let numeros = [1,23,34,5,6,7,8];
 
 console.log(numeros.length);
-console.log(numeros.['length']);
+console.log(numeros['length']);
 console.log(numeros[1]);
