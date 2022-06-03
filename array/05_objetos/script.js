9f2784007bd32993a836fd8eeb87d6de5ed77dfb
Diego leandro <diegolgaspar000@gmail.com>
2022-06-02 22:56:18 -0300

    estudo objetos

---
+++ array/05_objetos/script.js
@@ -0,0 +1,14 @@
+let cachorro = {
+    patas: 4,
+    nome: 'Fox',
+    latir: function(){
+        console.log("AU AU");
+    }
+};
+
+console.log(cachorro.patas);
+console.log(cachorro.nome);
+cachorro.latir();
+
+//{} = obj
+//[] = array
---
+++ array/06_objetos_criar_prop/script.js
@@ -0,0 +1,17 @@
+let pessoa = {
+    nome: "Diego",
+    idade: 29,
+    profissão: "Programador"
+}
+
+console.log(pessoa.nome);
+
+delete pessoa.nome;
+
+console.log(pessoa.nome);
+
+console.log(pessoa);
+
+pessoa.ano = 1993;
+
+console.log(pessoa.ano);
