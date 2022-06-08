967ac566d87eac91b3d36520e37f4e89f665b7aa
Diego leandro <diegolgaspar000@gmail.com>
2022-06-07 21:56:48 -0300

    método split e join

---
+++ array/20_split/script.js
@@ -0,0 +1,11 @@
+let frase = "Testando o método split";
+
+console.log(frase.split(" "));
+
+let palavras = frase.split(" ");
+
+console.log(palavras);
+
+let produtos = "Banana; aa uva aa Maça;Jaca; aa Pera;Bola; aa Tapete";
+
+console.log(produtos.split('aa'));
