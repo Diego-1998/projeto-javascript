5df0090de4b5b8656d4b5c1b44102f84c9196d11
Diego leandro <diegolgaspar000@gmail.com>
2022-06-09 22:47:46 -0300

    desafios javascript

---
+++ array/desafios_array/01_exercicio/script.js
@@ -0,0 +1,5 @@
+let numeros = [2,3,5,7,9];
+
+console.log(numeros[0]);
+console.log(numeros[3]);
+console.log(numeros[4]);
---
+++ array/desafios_array/02_exercicio/script.js
@@ -0,0 +1,6 @@
+let num = [2,4];
+
+let num2 = [10,12,15,30];
+
+console.log(num.length);
+console.log(num2.length);
