e0e4ad9a98f8c59be33fd49356f3134bfe04b8c5
Diego leandro <diegolgaspar000@gmail.com>
2022-06-10 23:35:38 -0300

    desafios 2 javascript

---
+++ array/desafios_array/04_exercicio/script.js
@@ -0,0 +1,10 @@
+let nome = ["Diego", "henry", "zidane", "matheus", "alice"];
+
+for( i = 0; i < nome.length; i++){
+    if(nome[i] == "zidane"){
+        console.log("nome encontrado");
+    }
+    else{
+        console.log("nome não encontrado");
+    }
+}
