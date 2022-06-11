f795be4676e54ad9da279554f2cb63eaf882e171
Diego leandro <diegolgaspar000@gmail.com>
2022-06-11 19:59:12 -0300

    desafios 3 javascript

---
+++ array/desafios_array/05_exercicio/script.js
@@ -0,0 +1,14 @@
+let nums = [1,2,3,4,5];
+let nums2 = [6,7];
+
+function verificar(arr){
+    if(nums.length >=5){
+        console.log("muitos elementos");
+    }
+    else{
+        console.log("Poucos elementos")
+    }
+}
+
+verificar(nums);
+verificar(nums2);
