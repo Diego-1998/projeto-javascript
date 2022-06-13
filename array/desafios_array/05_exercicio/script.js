dfdad8b6cfa6824e9cd39794ff816c30420c605f
Diego leandro <diegolgaspar000@gmail.com>
2022-06-12 23:26:39 -0300

    desafios 4 javascript

--- array/desafios_array/05_exercicio/script.js
+++ array/desafios_array/05_exercicio/script.js
@@ -1,8 +1,8 @@
-let nums = [1,2,3,4,5];
+let nums = [1,2,3,4,5,6,7];
 let nums2 = [6,7];
 
 function verificar(arr){
-    if(nums.length >=5){
+    if(arr.length >=5){
         console.log("muitos elementos");
     }
     else{
---
+++ array/desafios_array/06_exercicio/script.js
@@ -0,0 +1,5 @@
+let linguagens = ["Java", "Javascript", "php", "Ruby", "Go"];
+
+for( i =0; i <= linguagens.length; i++){
+    console.log(linguagens[i]);
+}
