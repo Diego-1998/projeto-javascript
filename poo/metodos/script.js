9089bb87d0ba2068c62572993dd38dc6c0a35881
Diego leandro <diegolgaspar000@gmail.com>
2022-06-15 22:48:32 -0300

    estudo sobre metodos

---
+++ poo/metodos/script.js
@@ -0,0 +1,11 @@
+const cachorro = {
+    latir: function(){
+        console.log("Auuuuuu");
+    },
+    rosnar: function(){
+        console.log("Grrrrr")
+    }
+}
+
+cachorro.latir();
+cachorro.rosnar();
