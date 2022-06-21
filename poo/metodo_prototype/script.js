5ead6fc9589424c2caba55bbb73b4083b228344d
Diego leandro <diegolgaspar000@gmail.com>
2022-06-20 22:38:19 -0300

    metodos com prototype

---
+++ poo/metodo_prototype/script.js
@@ -0,0 +1,17 @@
+function Cachorro(raca, patas, cor){
+    this.raca = raca;
+    this.patas = patas;
+    this.cor = cor;
+}
+
+Cachorro.prototype.uivar = function(){
+    console.log("Auuu");
+}
+
+Cachorro.prototype.latir = function() {
+    console.log("Au Au Au");
+}
+
+let husky = new Cachorro('Husky', 4, 'cinza');
+
+husky.uivar();
