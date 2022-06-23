f0e243da162a63baa6db432f3b68224d8b4968d7
Diego leandro <diegolgaspar000@gmail.com>
2022-06-22 23:38:17 -0300

    Symbol

---
+++ poo/symbol/script.js
@@ -0,0 +1,19 @@
+class Cachorro {
+    constructor(raca,cor){
+        this.raca = raca;
+        this.cor = cor;
+    }
+
+    latir() {
+        console.log("Au Au Au Au");
+    }
+}
+
+let patas = Symbol();
+
+Cachorro.prototype[patas] = 4;
+
+let labrador = new Cachorro('pitbull', 'Amarelo');
+
+//acessando symbol
+console.log(Cachorro.prototype[patas]);
